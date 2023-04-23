546cdbc77d3c8088456191c9c655d33d2bd176fc
Lawin0129 <56766256+Lawin0129@users.noreply.github.com>
2023-04-23 14:10:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -50,9 +50,9 @@ app.listen(PORT, () => {
 
 // if endpoint not found, return this error
 app.use((req, res, next) => {
-    res.status(404).json(error.createError(
+    error.createError(
         "errors.com.epicgames.common.not_found", 
         "Sorry the resource you were trying to find could not be found", 
-        undefined, 1004, undefined)
+        undefined, 1004, undefined, 404, res
     );
 });
