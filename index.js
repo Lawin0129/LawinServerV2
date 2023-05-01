082dae244f860e9a027ff26f9b7b6b52422bb208
Lawin0129 <56766256+Lawin0129@users.noreply.github.com>
2023-05-01 20:48:44 +0100

    Change rate limit to 40 requests per 30 seconds

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ mongoose.connection.on("error", err => {
     throw err;
 });
 
-app.use(rateLimit({ windowMs: 0.5 * 60 * 1000, max: 50 }));
+app.use(rateLimit({ windowMs: 0.5 * 60 * 1000, max: 40 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
