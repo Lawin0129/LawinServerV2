073490db49d8d8ca2c549ec6d88bfcc8c8923c8e
Finn <ernzerhofffinn@gmail.com>
2023-05-19 01:48:35 +0200

    Added checks for reporting

--- routes/reports.js
+++ routes/reports.js
@@ -3,26 +3,57 @@ const app = express.Router();
 
 const { verifyToken, verifyClient } = require("../tokenManager/tokenVerify.js");
 const User = require("../model/user.js");
-const config = require("../Config/config.json");
-const {Client, Intents} = require("discord.js");
-const Discord = require("discord.js");
 
-app.post("/fortnite/api/game/v2/toxicity/account/:reporter/report/:reportedPlayer", verifyToken, async (req, res) => {
+app.post("/fortnite/api/game/v2/toxicity/account/:reporter/report/:reportedPlayer", verifyToken, async (req, res, next) => {
+    try {
+        const reporter = req.params.reporter;
+        const reportedPlayer = req.params.reportedPlayer;
 
-    const reporter = req.params.reporter;
-    const reportedPlayer = req.params.reportedPlayer;
-    
-    let reporterData = await User.findOne({ accountId: reporter }).lean();
-    let reportedPlayerData = await User.findOne({ accountId: reportedPlayer }).lean();
-    
-    const reason = req.body.reason || 'No reason provided';
-    const details = req.body.details || 'No details provided';
-    const markedasknown = req.body.bUserMarkedAsKnown ? 'Yes' : 'No';
+        if (!reporter || !reportedPlayer) {
+            res.status(400).send({ success: false, error: 'Missing reporter or reportedPlayer' });
+            return;
+        }
 
-    const user = await User.findOneAndUpdate({ accountId: reportedPlayer } , { $inc: { reports: 1 } }, { new: true });
+        let reporterData;
+        let reportedPlayerData;
 
-    res.status(200).send({ "success": true });
+        try {
+            reporterData = await User.findOne({ accountId: reporter });
+            reportedPlayerData = await User.findOne({ accountId: reportedPlayer });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ success: false, error: 'Internal Server Error' });
+            return;
+        }
 
+        const reason = req.body.reason || 'No reason provided';
+        const details = req.body.details || 'No details provided';
+        const markedasknown = req.body.bUserMarkedAsKnown ? 'Yes' : 'No';
+
+        let user;
+
+        try {
+            user = await User.findOneAndUpdate(
+                { accountId: reportedPlayer },
+                { $inc: { reports: 1 } },
+                { new: true }
+            );
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ success: false, error: 'Internal Server Error' });
+            return;
+        }
+
+        if (!user) {
+            res.status(404).send({ success: false, error: 'User not found' });
+            return;
+        }
+
+        res.status(200).send({ success: true });
+
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 module.exports = app;
