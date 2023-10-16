d2cb97c7a51ac8d4370e28517ae311746f91f634
xLoigi <79259439+xLoigi@users.noreply.github.com>
2023-10-16 21:17:59 +0200

    Added SAC Functionality

---
+++ routes/affiliate.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const app = express.Router();
+
+const codes = require("./../model/saccodes.js");
+const Profile = require("../model/profiles.js");
+
+const { verifyToken, verifyClient } = require("../tokenManager/tokenVerify.js");
+
+app.get("/affiliate/api/public/affiliates/slug/:slug", async (req, res) => {
+    var slug = (req.params.slug.toLowerCase())
+
+    const code = (await codes.findOne({code_lower: slug}))  
+
+    var ValidCode = null;
+
+    if (code === null) { ValidCode = false} else { ValidCode = true}
+
+
+    if ( ValidCode == true ) {
+    return res.json({
+        "id": code.code_lower,
+        "slug": code.code_lower,
+        "displayName": code.code_lower,
+        "status": "ACTIVE",
+        "verified": false
+        });
+    };
+
+    if (ValidCode === false) {
+        res.status(404);
+        res.json({});
+    };
+});
+
+app.post("/fortnite/api/game/v2/profile/*/client/SetAffiliateName", verifyToken, async (req, res) => {
+    const profiles = await Profile.findOne({ accountId: req.params[0] });
+    let profile = profiles.profiles[req.query.profileId];
+
+    var ApplyProfileChanges = [];
+    var BaseRevision = profile.rvn || 0;
+    var QueryRevision = req.query.rvn || -1;
+    var StatChanged = false;
+
+
+    var slug = (req.body.affiliateName.toLowerCase());
+
+    const code = (await codes.findOne({code_lower: slug}))  
+
+    if (code == null) {        
+        res.status(404);
+        res.json({});
+    }
+
+    profile.stats.attributes.mtx_affiliate_set_time = new Date().toISOString();
+    profile.stats.attributes.mtx_affiliate = code.code_lower;
+
+    StatChanged = true;
+
+    if (StatChanged == true) {
+        profile.rvn += 1;
+        profile.commandRevision += 1;
+
+        ApplyProfileChanges.push({
+            "changeType": "statModified",
+            "name": "mtx_affiliate_set_time",
+            "value": profile.stats.attributes.mtx_affiliate_set_time
+        });
+
+        ApplyProfileChanges.push({
+            "changeType": "statModified",
+            "name": "mtx_affiliate",
+            "value": profile.stats.attributes.mtx_affiliate
+        });
+    }
+
+    if (QueryRevision != BaseRevision) {
+        ApplyProfileChanges = [{
+            "changeType": "fullProfileUpdate",
+            "profile": profile
+        }];
+    }
+
+    await profiles.updateOne({ $set: { [`profiles.${req.query.profileId}`]: profile} });
+
+    res.json({
+        "profileRevision": profile.rvn || 0,
+        "profileId": req.query.profileId || "common_core",  
+        "profileChangesBaseRevision": BaseRevision,
+        "profileChanges": ApplyProfileChanges,
+        "profileCommandRevision": profile.commandRevision || 0,
+        "serverTime": new Date().toISOString(),
+        "responseVersion": 1
+    })
+    res.end();
+});
+
+
+module.exports = app;
